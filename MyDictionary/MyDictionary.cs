8e0b452775bc65a5dd246ec3674dd620998b86b8
Samet-Cihangir <samet.cihangir@outlook.com>
2021-01-28 21:31:25 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        public List<TKey> _keys;
+        public List<TValue> _values;
+
+        public MyDictionary()
+        {
+            _keys = new List<TKey>();
+            _values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            foreach (TKey item in _keys)
+            {
+                if (item.Equals(key))
+                {
+                    Console.WriteLine("Hata aldınız , bu key daha önceden girimiştir.");
+                    return;
+                }
+
+            }
+            _keys.Add(key);
+            _values.Add(value);
+        }
+
+
+        public TValue GetValue(TKey key)
+        {
+            int index = _keys.IndexOf(key);
+            TValue value = _values[index];
+            return value;
+        }
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using MyDictionary;
+using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.Add("book", "kitap");
+            dictionary.Add("table", "tablo");
+            dictionary.Add("computer", "bilgisayar");
+
+
+            foreach (var item in dictionary._keys)
+            {
+                Console.WriteLine(item + " " + dictionary.GetValue(item));
+            }
+
+        }
+    }
+}
